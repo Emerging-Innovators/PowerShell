9ee0a1355359669b543d5cfab07b5db08ce958db
alex0918 <alex.mailme12@gmail.com>
2023-06-08 17:02:42 +0400

    PowerShell

---
+++ FSMO Roles Check.ps1
@@ -0,0 +1,17 @@
+﻿############### List of installed roles in my domain  ################
+
+# InfrastructureMaster 
+# PDCEmulator 
+# RIDMaster 
+# DomainNamingMaster 
+# Schemamaster 
+
+#-------------------Get domain level FSMO roles----------------------#
+
+Get-ADDomain | select InfrastructureMaster, PDCEmulator, RIDMaster
+
+#-------------------Get forest level FSMO roles----------------------# 
+
+Get-ADForest | select DomainNamingMaster, SchemaMaster
+
+#--------------------------------------------------------------------#
---
+++ FSMO Roles Transfer.ps1
@@ -0,0 +1,25 @@
+﻿#--------------------------------------------------------------------------------#
+               
+                             # FSMO Roles Transfer #
+
+#---------------------------Transfer PDCEmulator---------------------------------#
+
+Move-ADDirectoryServerOperationMasterRole -Identity "dc1" PDCEmulator
+
+#----------------------------Transfer RIDMaster----------------------------------#
+
+Move-ADDirectoryServerOperationMasterRole -Identity "dc1" RIDMaster
+
+#-----------------------Transfer InfrastrctureMaster-----------------------------#
+
+Move-ADDirectoryServerOperationMasterRole -Identity "dc1" Infrastructuremaster
+
+#------------------------Transfer DomainNamingMaster-----------------------------#
+
+Move-ADDirectoryServerOperationMasterRole -Identity "dc1" DomainNamingmaster
+
+#---------------------------Transfer SchemaMaster--------------------------------#
+
+Move-ADDirectoryServerOperationMasterRole -Identity "dc1" SchemaMaster
+
+#--------------------------------------------------------------------------------#
