5c556e0d1a04dfad6f843d49abd7bca1ee1bc69f
alex0918 <alex.mailme12@gmail.com>
2023-06-09 16:08:22 +0400

    Get AD Group-Admin-Members Properties

---
+++ AD/Get AD Group-Admin-Members Properties.ps1
@@ -0,0 +1,22 @@
+﻿########################################################################### Get AD Administrators list #####################################################################
+
+Get-ADGroupMember -Identity Administrators | Select-Object name, objectClass,distinguishedName | Export-CSV "C:\temp\ADGroupMember.csv" -NoTypeInformation -Encoding UTF8
+
+############################################################################# Get Enterprise Admin list ####################################################################
+
+Get-ADGroupMember -Identity 'Enterprise Admins' -Recursive | Export-CSV "C:\temp\Enterprise Admins.csv" -NoTypeInformation -Encoding UTF8 | Out-GridView
+
+############################################################################## Get-ADUser Properties #######################################################################
+
+Get-ADUser -identity alex.sg -properties *  | Export-csv -path "C:\temp\Enterprise Admins.csv" -NoTypeInformation -Encoding UTF8
+
+############################################################################## Get AD Group Members ########################################################################
+
+Get-ADGroup -Filter {GroupScope -eq "DomainLocal"} | Get-ADGroupMember | Select-Object name, objectClass,distinguishedName | Out-GridView
+
+############################################################## Domain User Lats Logon Info #################################################################################
+
+Get-ADUser -Identity SQLAdmin -Properties LastLogon | Select Name, @{Name='LastLogon';Expression={[DateTime]::FromFileTime($_.LastLogon)}}
+
+###########################################################################################################################################################################
+
